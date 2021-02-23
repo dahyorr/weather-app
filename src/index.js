da4437f8c617a1ab401c9112fa4740bf632689a2
Dayo Adebanjo <dahyor@github.com>
2021-02-23 01:12:47 +0100

    disabled redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import reduxThunk from 'redux-thunk'
 import reducers from './reducers'
 import App from './components/App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
-const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
+// const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)))
+
+const store = createStore(reducers, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
     <Provider store={store}>
